0ff87bc6ab8f04f3897174088b401cf04a8496d0
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-08-25 15:17:16 -0400

    add SUNKEN relief to display label

--- Tkinter-Tutorial/step4_error_handling.py
+++ Tkinter-Tutorial/step4_error_handling.py
@@ -20,7 +20,7 @@ def std_btn(text, bg, row, col, width=7, height=2, font=('Franklin Gothic Book',
     return btn.grid(row=row, column=col, padx=4, pady=4)
 
 tk.Label(canvas, text='PyDataMath-II', anchor='e', bg='#272533', fg='white', font=('Franklin Gothic Book', 14, 'bold')).grid(row=0, columnspan=4, sticky='ew', padx=4, pady=2)
-tk.Label(canvas, textvariable=display_text, anchor='e', bg='black', fg='red', font=('Digital-7',47)).grid(row=1, columnspan=4, sticky='ew', padx=4, pady=2)
+tk.Label(canvas, textvariable=display_text, anchor='e', bg='black', fg='red', relief=tk.SUNKEN, font=('Digital-7',47)).grid(row=1, columnspan=4, sticky='ew', padx=4, pady=2)
 std_btn("C", TAN, 2, 0),   std_btn("CE", TAN, 2, 1),  std_btn("%", TAN, 2, 2),   std_btn("/", TAN, 2, 3)
 std_btn("7", WHITE, 3, 0), std_btn("8", WHITE, 3, 1), std_btn("9", WHITE, 3, 2), std_btn("*", TAN, 3, 3)
 std_btn("4", WHITE, 4, 0), std_btn("5", WHITE, 4, 1), std_btn("6", WHITE, 4, 2), std_btn("-", TAN, 4, 3)
