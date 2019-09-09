713c1059bc3d769faeb07f577b0774387120e779
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-09-09 07:19:57 -0400

    update

--- calculator_sg.py
+++ calculator_sg.py
@@ -1,40 +1,28 @@
 import PySimpleGUI as sg 
 
-# button colors
-BW = ("black","#F8F8F8") # black/white
-BT = ("black","#F1EABC") # black/tan
-BO = ("black","#ECA527") # black/orange
-
-def Button(name, color, size=(7, 2), **kwargs):
-    ''' return an sg button with default parameters '''    
-    button = sg.Button(name, size=size, button_color=color, font=('Franklin Gothic Book', 24), **kwargs)
-    return button
+# default settings
+bw = {'size':(7,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#F8F8F8")}
+bt = {'size':(7,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#F1EABC")}
+bo = {'size':(15,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#ECA527"), 'focus':True}
 
 layout = [
     [sg.Text('PyDataMath-II', size=(50,1), justification='right', background_color="#272533", text_color='white', font=('Franklin Gothic Book', 14, 'bold'))],
     [sg.Text('0.0000', size=(18,1), justification='right', background_color='black', text_color='red', font=('Digital-7',48), relief='sunken', key="_DISPLAY_")],
-    [Button('C',BT), Button('CE',BT), Button('%',BT), Button("/",BT)],
-    [Button('7',BW), Button('8',BW), Button('9',BW), Button("*",BT)],
-    [Button('4',BW), Button('5',BW), Button('6',BW), Button("-",BT)],
-    [Button('1',BW), Button('2',BW), Button('3',BW), Button("+",BT)],    
-    [Button('0',BW), Button('.',BW), Button('=',BO, size=(15,2), focus=True)],
+    [sg.Button('C',**bt), sg.Button('CE',**bt), sg.Button('%',**bt), sg.Button("/",**bt)],
+    [sg.Button('7',**bw), sg.Button('8',**bw), sg.Button('9',**bw), sg.Button("*",**bt)],
+    [sg.Button('4',**bw), sg.Button('5',**bw), sg.Button('6',**bw), sg.Button("-",**bt)],
+    [sg.Button('1',**bw), sg.Button('2',**bw), sg.Button('3',**bw), sg.Button("+",**bt)],    
+    [sg.Button('0',**bw), sg.Button('.',**bw), sg.Button('=',**bo)],
 ]
 window = sg.Window('PyDataMath-II', layout=layout, background_color="#272533", size=(580, 660))
 
 ''' calculator functions '''
-# global variables
-front = []
-back = []
-decimal = False
-operator = ''
-x_val = 0.0
-y_val = 0.0
-result = 0.0
+var = {'front':[], 'back':[], 'decimal':False, 'x_val':0.0, 'y_val':0.0, 'result':0.0, 'operator':''}
 
 # helper functions
 def format_number():
     ''' create a consolidated string of numbers from front and back lists '''
-    return float(''.join(front) + '.' + ''.join(back))
+    return float(''.join(var['front']) + '.' + ''.join(var['back']))
 
 def update_display(display_value):
     ''' update the calculator display after an event click '''
@@ -46,37 +34,37 @@ def update_display(display_value):
 # click events
 def number_click(event):
     ''' number button button click event '''
-    global front, back
-    if decimal:
-        back.append(event)
+    global var
+    if var['decimal']:
+        var['back'].append(event)
     else:
-        front.append(event)
+        var['front'].append(event)
     update_display(format_number())
     
 def clear_click():
-    ''' ce or c button click event '''
-    global front, back, decimal 
-    front.clear()
-    back.clear()
-    decimal = False 
+    ''' CE or C button click event '''
+    global var
+    var['front'].clear()
+    var['back'].clear()
+    var['decimal'] = False 
 
 def operator_click(event):
     ''' + - / * button click event '''
-    global operator, x_val
-    operator = event
+    global var
+    var['operator'] = event
     try:
-        x_val = format_number()
+        var['x_val'] = format_number()
     except:
-        x_val = result
+        var['x_val'] = var['result']
     clear_click()
 
 def calculate_click():
     ''' equals button click event '''
-    global y_val, result 
-    y_val = format_number()
+    global var
+    var['y_val'] = format_number()
     try:
-        result = eval(str(x_val) + operator + str(y_val))
-        update_display(result)
+        var['result'] = eval(str(var['x_val']) + var['operator'] + str(var['y_val']))
+        update_display(var['result'])
         clear_click()    
     except:
         update_display("ERROR! DIV/0")
@@ -88,15 +76,15 @@ while True:
         break
     if event in ['0','1','2','3','4','5','6','7','8','9']:
         number_click(event)
-    if event in ['c','ce']:
+    if event in ['C','CE']:
         clear_click()
         update_display(0.0)
-        result = 0.0
+        var['result'] = 0.0
     if event in ['+','-','*','/']:
         operator_click(event)
     if event == '=':
         calculate_click()
     if event == '.':
-        decimal = True
+        var['decimal'] = True
     if event == '%':
-        update_display(result / 100.0)
+        update_display(var['result'] / 100.0)
--- calculator_tk.py
+++ calculator_tk.py
@@ -1,9 +1,9 @@
 # PyDataMath-II Calculator
 import tkinter as tk
 
-# button colors
 WHITE = "#F8F8F8" # black/white
 TAN = "#F1EABC" # black/tan
+var = {'front':[], 'back':[], 'decimal':False, 'x_val':0.0, 'y_val':0.0, 'result':0.0, 'operator':''}
 
 root = tk.Tk()
 root.title('PyDataMath-II')
@@ -31,32 +31,22 @@ rtn_btn = tk.Button(canvas, text='=', bg='#ECA527', width=15, height=2, font=('F
 rtn_btn.focus()
 rtn_btn.grid(row=6, column=2, columnspan=2, padx=4, pady=4)
 
-''' calculator functions '''
-# global variables
-# design pattern :: 1,234.57 --> [front] . [back]
-front = [] 
-back = []
-decimal = False
-x_val = 0.0
-y_val = 0.0
-result = 0.0
-operator = ''
-
+# calculator functions
 def event_click(event):
-    global operator, decimal, result
+    global var
     if event in ['CE','C']:
         clear_click()
         update_display(0.0)
-        operator = ''
-        result = 0.0
+        var['operator'] = ''
+        var['result'] = 0.0
     if event in ['0','1','2','3','4','5','6','7','8','9']:
         number_click(event)
     if event in ['*','/','+','-']:
         operator_click(event)
     if event == '.':
-        decimal = True 
+        var['decimal'] = True 
     if event == '%':
-        update_display(result / 100.0)
+        update_display(var['result'] / 100.0)
     if event == '=':
         calculate_click()
 
@@ -71,68 +61,57 @@ def update_display(display_value):
 
 def format_number():
     ''' create a consolidated string of numbers from front and back number lists '''
-    return ''.join(front) + '.' + ''.join(back)
+    return ''.join(var['front']) + '.' + ''.join(var['back'])
 
 # click events
 def number_click(event):
     ''' add digit to front or back list when clicked '''
-    global front, back
-    if decimal:
-        back.append(event)
+    global var
+    if var['decimal']:
+        var['back'].append(event)
     else:
-        front.append(event)
+        var['front'].append(event)
 
     display_value = float(format_number())
     update_display(display_value)
 
 def clear_click():
     ''' clear contents of front and back list, reset display, and reset decimal flag '''
-    global front, back, decimal
-    front.clear()
-    back.clear()
-    decimal = False
+    global var
+    var['front'].clear()
+    var['back'].clear()
+    var['decimal'] = False
 
 def operator_click(event):
     ''' set the operator based on the event button, this may also trigger a calculation in the event
         that the result is used in a subsequent operation '''
-    global operator, x_val
-    operator = event
+    global var
+    var['operator'] = event
     try: # if no x_val exists, use prior result
-        x_val = float(format_number())
+        var['x_val'] = float(format_number())
     except:
-        x_val = result
+        var['x_val'] = var['result']
     clear_click()
 
 def calculate_click():
     ''' attempt to perform operation on x and y variables if exist '''
-    global x_val, y_val, result
-
+    global var
     # check for x value
-    if not x_val:
+    if not var['x_val']:
         return 
     try: # check for y value
-        y_val = float(format_number())
+        var['y_val'] = float(format_number())
     except:
-        y_val = 0.0
+        var['y_val'] = 0.0
     try: # check for division by zero
-        result = float(eval(str(x_val) + operator + str(y_val)))
-        update_display(result)
+        var['result'] = float(eval(str(var['x_val']) + var['operator'] + str(var['y_val'])))
+        update_display(var['result'])
     except ZeroDivisionError:
         error = "ERROR! DIV/0"
-        x_val = 0.0
+        var['x_val'] = 0.0
         clear_click()
         update_display(error)
 
     clear_click()
 
 root.mainloop()
-
-''' ERROR Handling
-    - division by zero errors (calculate_click)
-    - click = without having a y value (calculate_click)
-    - click = without having any values to operate on (calculate_click)
-
-    OTHER Features
-    - perform calculation on prior result (operator_click)
-
-'''
