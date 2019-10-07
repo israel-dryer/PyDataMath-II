fe915dbbc5888c0684886a3ec38fe44d25ecc36f
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-10-07 15:46:29 -0400

    added typing, updated comments

--- calculator_sg.py
+++ calculator_sg.py
@@ -1,11 +1,21 @@
+'''
+  A simple Python calculator gui application based loosely on the Texas Instruments DataMath II 
+  produced circa 1975
+  
+  Author:     Israel Dryer
+  Email:      israel.dryer@gmail.com
+  Modified:   2019-10-07
+  
+'''
 import PySimpleGUI as sg 
 
-# default settings
+##-----DEFAULT SETTINGS----------------------------------##
 bw = {'size':(7,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#F8F8F8")}
 bt = {'size':(7,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#F1EABC")}
 bo = {'size':(15,2), 'font':('Franklin Gothic Book', 24), 'button_color':("black","#ECA527"), 'focus':True}
 
-layout = [
+##-----WINDOW AND LAYOUT---------------------------------##
+layout: list = [
     [sg.Text('PyDataMath-II', size=(50,1), justification='right', background_color="#272533", 
         text_color='white', font=('Franklin Gothic Book', 14, 'bold'))],
     [sg.Text('0.0000', size=(18,1), justification='right', background_color='black', text_color='red', 
@@ -14,28 +24,34 @@ layout = [
     [sg.Button('7',**bw), sg.Button('8',**bw), sg.Button('9',**bw), sg.Button("*",**bt)],
     [sg.Button('4',**bw), sg.Button('5',**bw), sg.Button('6',**bw), sg.Button("-",**bt)],
     [sg.Button('1',**bw), sg.Button('2',**bw), sg.Button('3',**bw), sg.Button("+",**bt)],    
-    [sg.Button('0',**bw), sg.Button('.',**bw), sg.Button('=',**bo, bind_return_key=True)],
+    [sg.Button('0',**bw), sg.Button('.',**bw), sg.Button('=',**bo, bind_return_key=True)]
 ]
-window = sg.Window('PyDataMath-II', layout=layout, background_color="#272533", size=(580, 660), return_keyboard_events=True)
 
-''' calculator functions '''
-var = {'front':[], 'back':[], 'decimal':False, 'x_val':0.0, 'y_val':0.0, 'result':0.0, 'operator':''}
+window: object = sg.Window('PyDataMath-II', layout=layout, background_color="#272533", size=(580, 660), return_keyboard_events=True)
 
-# helper functions
-def format_number():
-    ''' create a consolidated string of numbers from front and back lists '''
+
+##----CALCULATOR FUNCTIONS-------------------------------##
+
+var: dict = {'front':[], 'back':[], 'decimal':False, 'x_val':0.0, 'y_val':0.0, 'result':0.0, 'operator':''}
+
+
+#-----HELPER FUNCTIONS
+def format_number() -> float:
+    ''' Create a consolidated string of numbers from front and back lists '''
     return float(''.join(var['front']) + '.' + ''.join(var['back']))
 
-def update_display(display_value):
-    ''' update the calculator display after an event click '''
+
+def update_display(display_value: str):
+    ''' Update the calculator display after an event click '''
     try:
         window['_DISPLAY_'].update(value='{:,.4f}'.format(display_value))
     except:
         window['_DISPLAY_'].update(value=display_value)
 
-# click events
-def number_click(event):
-    ''' number button button click event '''
+
+#-----CLICK EVENTS
+def number_click(event: str):
+    ''' Number button button click event '''
     global var
     if var['decimal']:
         var['back'].append(event)
@@ -43,6 +59,7 @@ def number_click(event):
         var['front'].append(event)
     update_display(format_number())
     
+
 def clear_click():
     ''' CE or C button click event '''
     global var
@@ -50,7 +67,8 @@ def clear_click():
     var['back'].clear()
     var['decimal'] = False 
 
-def operator_click(event):
+
+def operator_click(event: str):
     ''' + - / * button click event '''
     global var
     var['operator'] = event
@@ -60,8 +78,9 @@ def operator_click(event):
         var['x_val'] = var['result']
     clear_click()
 
+
 def calculate_click():
-    ''' equals button click event '''
+    ''' Equals button click event '''
     global var
     var['y_val'] = format_number()
     try:
@@ -72,6 +91,8 @@ def calculate_click():
         update_display("ERROR! DIV/0")
         clear_click()
 
+
+#-----MAIN EVENT LOOP------------------------------------##
 while True:
     event, values = window.read()
     print(event)
